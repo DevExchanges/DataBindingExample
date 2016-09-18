f107819cc4fe6dfdaec53adca44b6c12c7401285
DevExchanges <wandered.vip.007@gmail.com>
2016-09-18 12:06:56 +0700

    update data binding

--- app/src/main/java/info/devexchanges/simpledatabinding/Cat.java
+++ app/src/main/java/info/devexchanges/simpledatabinding/Cat.java
@@ -1,31 +1,45 @@
 package info.devexchanges.simpledatabinding;
 
-public class Cat {
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
+public class Cat extends BaseObservable {
     private String name;
     private int age;
     private String owner;
 
+    public Cat(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Bindable
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+        notifyPropertyChanged(info.devexchanges.simpledatabinding.BR.name);
     }
 
+    @Bindable
     public int getAge() {
         return age;
     }
 
     public void setAge(int age) {
         this.age = age;
+        notifyPropertyChanged(info.devexchanges.simpledatabinding.BR.age);
     }
 
+    @Bindable
     public String getOwner() {
         return owner;
     }
 
     public void setOwner(String owner) {
         this.owner = owner;
+        notifyPropertyChanged(info.devexchanges.simpledatabinding.BR.owner);
     }
 }
--- app/src/main/java/info/devexchanges/simpledatabinding/MainActivity.java
+++ app/src/main/java/info/devexchanges/simpledatabinding/MainActivity.java
@@ -1,13 +1,33 @@
 package info.devexchanges.simpledatabinding;
 
-import android.support.v7.app.AppCompatActivity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import info.devexchanges.simpledatabinding.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Cat cat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActivityMainBinding mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        cat = new Cat("Tom", 2);
+        mainBinding.setCat(cat);
+
+        OnClickHandler handlers = new OnClickHandler();
+        mainBinding.setHandlers(handlers);
+    }
+
+    public class OnClickHandler {
+        public void onUpdateCat(View view) {
+            cat.setName("Tom Tom");
+            cat.setOwner("Hong Thai");
+        }
     }
 }
